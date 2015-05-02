102ed40fe5ba4caa65ee6856505d17aa28f4fe01
unknown <neil.kilbride@ES00702.esendex.com>
2015-05-02 17:06:25 +0100

    First commit

---
+++ CrowdPlay/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CrowdPlay.Startup))]
+namespace CrowdPlay
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
